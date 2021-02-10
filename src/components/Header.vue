31cec9c08652fcd5e37f3a2aad4754edfb9334ae
gastonm3112 <gastonm3112@gmail.com>
2021-02-10 08:53:22 -0300

    Creadas liks a rutas en el Navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,22 +17,12 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a
-              class="nav-link disabled"
-              href="#"
-              tabindex="-1"
-              aria-disabled="true"
-              >Disabled</a
+            <router-link class="nav-link active" aria-current="page" to="/"
+              >Home</router-link
             >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/About">About</router-link>
           </li>
         </ul>
       </div>
