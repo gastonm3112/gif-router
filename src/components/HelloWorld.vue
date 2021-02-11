5cbf569c761e1da8ac1dd854151295d329474897
gastonm3112 <gastonm3112@gmail.com>
2021-02-11 09:19:47 -0300

    Card agregada a Home

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mt-3">
+    <div class="card" style="width: 100%;">
+      <img
+        src="../assets/celular.jpg"
+        class="card-img-top img-fluid"
+        alt="Imagen de un celular con redes sociales en pantalla"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Gif Router</h5>
+        <p class="card-text">
+          Sitio realizado para reforzar conocimientos sobre Rutas, APIS,
+          Componentes y más en Vue 3
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
