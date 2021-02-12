a5e919aa3b47cd6419df9d4997d832d4981a7fe3
gastonm3112 <gastonm3112@gmail.com>
2021-02-12 09:55:01 -0300

    Peticion a API Giphy realizada satisfactoriamente

--- src/views/Gifs.vue
+++ src/views/Gifs.vue
@@ -7,10 +7,20 @@
 
 <script>
 export default {
+  data: () => ({
+    gifs: null,
+  }),
   mounted() {
-    const res = this.axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then(({ data }) => console.log(data));
+    this.getGifs();
+  },
+  methods: {
+    async getGifs() {
+      const res = await this.axios.get(
+        "https://api.giphy.com/v1/gifs/random?api_key=YCZzQAunZxEv7WQPi1WaK0PIZ2wqMDvW"
+      );
+
+      console.log(res);
+    },
   },
 };
 </script>
