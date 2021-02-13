28a3c0e27a43d5aed52d13f8fec04d8f0239638b
gastonm3112 <gastonm3112@gmail.com>
2021-02-13 11:06:29 -0300

    Renderizado de Gifs realizado con exito!

--- src/views/Gifs.vue
+++ src/views/Gifs.vue
@@ -5,8 +5,8 @@
     <hr />
 
     <div class="row">
-      <div class="col-sm-12">
-        <gif-card :data="gifs" class="m-3 w-75" />
+      <div class="col-sm-12" v-for="gif in gifs" :key="gif.id">
+        <gif-card :data="gif" class="m-3 w-75" />
       </div>
     </div>
   </div>
@@ -27,15 +27,15 @@ export default {
     async getGifs(search) {
       //Obtiene Gifs desde la API de GIPHY
       const { data } = await this.axios.get(
-        "https://api.giphy.com/v1/gifs/random?api_key=YCZzQAunZxEv7WQPi1WaK0PIZ2wqMDvW"
+        "https://api.giphy.com/v1/gifs/search?q=goku&api_key=YCZzQAunZxEv7WQPi1WaK0PIZ2wqMDvW"
       );
 
       //console.log(data);
 
       this.gifs = data.data; //gifs toma el valor de data
 
-      console.log(this.gifs);
-      console.log(search);
+      // console.log(this.gifs);
+      // console.log(search);
     },
   },
 };
