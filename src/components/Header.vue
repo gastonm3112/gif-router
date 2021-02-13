9694c5c4b8cef7709eb9ef7d9118856840bcdbd4
gastonm3112 <gastonm3112@gmail.com>
2021-02-13 20:57:59 -0300

    Quitada class active de Home en el Navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient">
     <div class="container-fluid">
       <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-      <router-link class="navbar-brand" to="/">Gif Routes</router-link>
+      <router-link class="navbar-brand" to="/">Gif Router</router-link>
       <button
         class="navbar-toggler"
         type="button"
@@ -17,7 +17,7 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="/"
+            <router-link class="nav-link" aria-current="page" to="/"
               >Home</router-link
             >
           </li>
