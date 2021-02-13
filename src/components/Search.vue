b2a95be8e06395d8b41318bedb9ec7781c6fcf09
gastonm3112 <gastonm3112@gmail.com>
2021-02-13 10:06:13 -0300

    Form modificado para utilizar lo necesario

---
+++ src/components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <form>
+    <div class="mb-3">
+      <label for="categoria" class="form-label">Categoría</label>
+      <input
+        type="text"
+        class="form-control"
+        id="categoria"
+        aria-describedby="categoriaHelp"
+      />
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
