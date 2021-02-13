860c6b4329808c329c0b48fdffa5e9e3047b5092
gastonm3112 <gastonm3112@gmail.com>
2021-02-13 12:43:08 -0300

    Modificada Card de la vista Home

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="container mt-3">
     <div class="card" style="width: 100%;">
-      <img
-        src="../assets/celular.jpg"
-        class="card-img-top img-fluid"
-        alt="Imagen de un celular con redes sociales en pantalla"
-      />
       <div class="card-body">
         <h5 class="card-title">Gif Router</h5>
         <h6 class="card-subtitle mb-2 text-muted">Hecho por Gastón Martinez</h6>
